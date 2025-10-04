c656d2ca87207a94884b4b8bef4b110b3722e965
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-10-04 09:12:02 +0300

    refactor(api):change Cors settings

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,9 +11,8 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
     allowedHeaders: '*',
   });
   await app.listen(process.env.PORT ?? 3000);
